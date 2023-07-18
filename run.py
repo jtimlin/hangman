fab80318134590fda4a9589dc7bda9b497e0ddb9
Joonas Timlin <130164187+jindah@users.noreply.github.com>
2023-07-18 21:32:12 +0200

    Add Words.py and import random and time modules

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import time
+from words import word_list
+
+
+def get_word():
+    """"
+    Returns a random word from the word list
+    """
+    word = random.choice(word_list)
+    return word.lower()
+
+
+def hangman(word):
